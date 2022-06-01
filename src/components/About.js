04f72bafe766b1b44e2ea98ab627f3bd1d6cd3c5
Bishnu <bishnuthapako@gmail.com>
2022-05-31 21:46:15 -0400

    Completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,29 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
 
-function About() {
+
+
+
+function About(props) { 
+//  function bio(props){
+//    const bio = props.bio;
+
+//    return <p></p>
+
+//  }
+  
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2 id="about">About Me</h2>
+      {props.bio ? <p>{props.bio}</p>:null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+
+      <Links github = {user.links.github}
+      linkedin = {user.links.linkedin}/>
     </div>
+    
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { username, city } from "../data/user";
+
 
 function Home(props) {
+
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
+      <h1 style= {{color:"firebrick"}} >
         {props.name} is a Web Developer from {props.city}
       </h1>
     </div>
   );
 }
 
-export default Home;
+export default Home
